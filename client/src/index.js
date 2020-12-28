21f4fb0db3ed64ad8b352cc57f4e1da3d7e9a27f
maurya512 <patelmaurya0512@gmail.com>
2020-12-27 19:03:14 -0500

    created a client side folder that is predominantly going to be the front end of the app

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+// connect the react application to index.html
+// import dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import components
+import App from './App';
+
+// connecting the div with and id of root
+ReactDOM.render(<App />, document.getElementById('root'))
