4cea88b182b9e6ff6d971579d8cd82daca204a04
maurya512 <patelmaurya0512@gmail.com>
2020-12-29 22:24:12 -0500

    designed the basic layout of the page and imported some components into it

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 // import dependencies
 
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+// import logo from the folder 
+import myworld from './images/myworld.png'
+
+// import components 
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>My World</Typography>
+                <img src={myworld} alt='my world' height='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
