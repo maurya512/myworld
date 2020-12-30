20f5ae53479ac29a1fc89f11cb2eb5fb09559bdc
maurya512 <patelmaurya0512@gmail.com>
2020-12-29 22:23:28 -0500

    imported files

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// import files
+import postRoutes from './routes/posts.js';
+
 // initialize the app
 const app = express();
 
+// setup the starting path for all routes
+// every route within postRoutes will start with 'posts
+app.use('/posts', postRoutes);
 // set a limit to upload a jpeg to 30 mb
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
