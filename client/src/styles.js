3682253a1f5da87d3a1deec19d42ca8c051fed1d
maurya512 <patelmaurya0512@gmail.com>
2020-12-29 22:51:48 -0500

    added styling for the main homepage

---
+++ client/src/styles.js
@@ -0,0 +1,20 @@
+// import dependencies 
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    // styling elements for the homepage/landing page
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }, 
+    heading: {
+        color: 'rgba(0,183,255,1)',
+    },
+    image: {
+        marginLeft: '15px',       
+    },
+}));
